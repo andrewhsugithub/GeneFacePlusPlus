980ee298038e6456aee8ff1106eeea94b1c5ab05
andrewhsugithub <andrew1mail@gmail.com>
2024-07-21 00:00:48 +0800

    feat: add a start.sh to start the environment quickly and open webui

---
+++ start.sh
@@ -0,0 +1,8 @@
+﻿#! /bin/bash
+
+echo "source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate"
+source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate
+echo "conda activate geneface"
+conda activate geneface
+echo "python inference/app_genefacepp.py --a2m_ckpt=checkpoints/audio2motion_vae --head_ckpt= --torso_ckpt=checkpoints/motion2video_nerf/may_torso"
+python inference/app_genefacepp.py --a2m_ckpt=checkpoints/audio2motion_vae --head_ckpt= --torso_ckpt=checkpoints/motion2video_nerf/may_torso
